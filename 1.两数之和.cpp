c034f66b7560bd2755b6bd7ace4a1e340f9e7dc4
ljw <805474954@qq.com>
2019-10-21 10:58:58 +0800

    11

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -4,7 +4,7 @@
  * @作者: jianwen
  * @Date: 2019-10-20 09:38:57
  * @LastEditors: jianwen
- * @LastEditTime: 2019-10-20 09:38:57
+ * @LastEditTime: 2019-10-21 10:57:29
  */
 /*
  * @lc app=leetcode.cn id=1 lang=cpp
@@ -15,10 +15,11 @@
 // @lc code=start
 # include <vector>
 using namespace std;
-
+// 暴力匹配法，两个for循环遍历整个数组
+// Vector作为函数的参数或者返回值时，&绝对不能少
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target) {  
         int i,j;
         for(i=0;i<nums.size()-1;i++)
         {
@@ -26,11 +27,11 @@ public:
             {
                 if(nums[i]+nums[j]==target)
                 {
-                   return {i, j};
+                   return vector<int>{i,j};
                 }
             }
         }
-        return {i, j};
+        return vector<int>{};
     };
 };
 // @lc code=end
